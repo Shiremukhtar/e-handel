e1bd27b27501cff60efc235fc86ebdf15a350c8f
cuncu <cuncu@DESKTOP-PQ5FM4I>
2022-01-25 10:32:57 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./containers/Header";
+import ProductDetail from "./containers/ProductDetail";
+import ProductListing from "./containers/ProductListing";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />}></Route>
+          <Route path="/product/:pruductId" element={<ProductDetail />}></Route>
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/containers/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Product = () => {
+    const products = useSelector((state) => state.allProducts.products);
+    const { id, title } = products[0];
+  return (
+  <div className='four column wide ct'>
+      <div className='ui link cards'>
+          <div className='card'>
+              <div className='image'></div>
+              <div className='content'>
+                  <div className='header'>{title}</div>
+              </div>
+          </div>
+      </div>
+  </div>);
+};
+
+export default Product;
